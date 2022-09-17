53b461bea3d63f0b9c0bb9851332561174b5803a
apoorvcodes <tofu.develops@gmail.com>
2022-09-17 17:41:33 +0530

    feat: list done

--- routes/list.go
+++ routes/list.go
@@ -1,7 +1,16 @@
 package routes
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gominima/minima"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type KalaKritiData struct {
@@ -16,15 +25,31 @@ type DataArray struct {
 
 func ListHandler() minima.Handler {
 	return func(res *minima.Response, req *minima.Request) {
-		res.OK().JSON(&DataArray{
-			Data: []*KalaKritiData{
-				{
-					Date:     123,
-					Toxic:    true,
-					Readings: "abc",
-					Level:    "Healthy",
-				},
-			},
-		})
+		client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("DATABASE_URL")))
+	if err != nil {
+		fmt.Print("ERROR 1")
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			fmt.Print("ERROR 2")
+			panic(err)
+		}
+	}()
+    
+	// Ping the primary
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		fmt.Print("ERROR 3")
+		panic(err)
+	}
+	cursor, err := client.Database("Data").Collection("kala").Find(context.TODO(), bson.M{})
+if err != nil {
+    log.Fatal(err)
+}
+var data []*KalaKritiData
+if err = cursor.All(context.TODO(), &data); err != nil {
+    log.Fatal(err)
+}
+	res.OK().JSON(data)
 	}
 }
